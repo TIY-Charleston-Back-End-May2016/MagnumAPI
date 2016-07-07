61c1a8f4b126f705fa91a70e43f44b59e713e6ef
Luke Segars <luke@theironyard.com>
2016-07-07 12:07:57 -0400

    Modified code to render tomalikes and send favs.

--- public/js/app.js
+++ public/js/app.js
@@ -25,8 +25,19 @@ module.exports = function (app) {
 },{}],2:[function(require,module,exports){
 
 module.exports = function (app) {
-    app.controller('TomALikeController', ['$scope', 'TomALikeService', function ($scope, TomALikeService) {
+    app.controller('TomALikeController', ['$scope', '$http', 'TomALikeService', function ($scope, $http, TomALikeService) {
         $scope.tomalikes = TomALikeService.getTomALikes();
+
+        $scope.fav = function (tom, looksLike) {
+            $http({
+                url: '/favs',
+                method: 'post',
+                data: {
+                    tomId: tom.id,
+                    looksLikeTom: looksLike,
+                }
+            });
+        };
     }]);
 };
 },{}],3:[function(require,module,exports){
